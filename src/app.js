40d1dff8317b00b9ef88305d5f0c5f327f6ff9ab
Autumn Fjeld <autumn@talentbin.com>
2017-05-28 16:20:49 -0700

    Testing research layouts; extended-layout display; create pug folder

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 (function ($) {
 
     "use strict";
@@ -16,9 +17,11 @@
 
         // jquery backstretch slideshow
         $("#home").backstretch([
-          "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/bubble1.jpg",
-          "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/bubble3.jpg",
-          "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/bath-pouf.png"
+            "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/banner-images/Si_Al2O3_Ar_rotated_zoomed_in_1920x1280.jpg",
+            // "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/banner-images/HardCarbon_Sized_1920x1280.png",
+            // "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/bubble1.jpg",
+            // "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/bubble3.jpg",
+            "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/bubble6.jpg"
           ], {duration: 2000, fade: 750});
         
 
@@ -36,5 +39,68 @@
 
         // WOW Animation js
         new WOW({ mobile: false }).init();
+
+        //Handle research pages
+        $('#research').on('click', '.research-click', function(ev){
+            console.log('ev', ev);
+            console.log('ev', ev.target['data-project']);
+            var path = $(this).attr('data-project');
+            console.log('path', path);
+            var html = window.app.pugCompile.researchPagePugFunc({title: 'Fucking YES',teaser: 'Lots of text'});
+            // $('#main-page').hide();
+            // $('#research-page').append(html);
+            $('#expanded-research').append(html);
+            $('#expanded-research').slideDown('slow');
+            // $('#expanded-research').animate({
+            // opacity: 0.25,
+            // height: toggl
+
+            //compile pug function and display in new page
+
+        });
+
+        // console.log('  window.app.pugCompile.researchPagePugFunc',   window.app.pugCompile.researchPagePugFunc({title: 'Fucking YES',teaser: 'Lots of text'}));
+
+
+
+    $('.research-popup').magnificPopup({
+        type:'inline',
+        // gallery: true,
+        inline: {
+            // Define markup. Class names should match key names.
+            markup: '<div class="white-popup">' +
+
+                    '</div>'
+        },
+        gallery: {
+            enabled: true
+        },
+
+        callbacks: {
+            open: function() {
+              console.log('open  this', this);
+            },
+        //     // markupParse: function(template, values, item) {
+        //     //     // optionally apply your own logic - modify "template" element based on data in "values"
+        //     //     // console.log('Parsing:', template, values, item);
+        //     // },
+        //     //gets the source element
+            elementParse: function(item) {
+                console.log('elementParse', item);
+                var project =  $(item.el).attr('data-project');
+                console.log( $(item.el).attr('data-project'))
+                item.src=  '<div class="white-popup">' +
+                                "<h1> Hi Aut </h1>" +
+                                "<h1>"+ project +" </h1>" +
+                            '</div>' ;
+            },
+        //     change: function() {
+        //         console.log('Content changed');
+        //         console.log(this.content); // Direct reference to your popup element
+        //     }
+        }
+    });
+
+
 
 })(jQuery);
--- src/data.js
+++ src/data.js
@@ -17,11 +17,12 @@ var about = {
 
 var researchProjects = [
     {
-        title : "Engineering thermal properties in MOFs",
+        title: "Engineering thermal properties in MOFs",
+        href: "engineering-thermal-properties-in-mofs",
         imageUrl: "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/irmof-3-struct.thumbnail.png",
         imageTitle: "",
-        teaser: "Write a sexy 1-2 sentence teaser that must tantalize the reader to make them follow the link and read more.",
-        text: [
+        teaser: "MOFs sexy 1-2 sentence teaser that must tantalize the reader to make them follow the link and read more.",
+        expandedText: [
             "Metal-organic-frameworks (MOFs) are astounding new materials with open molecular-trellis structures. Because these materials are structurally so different from traditional  monolithic crystalline solids, they abound with opportunities for engineering thermal properties. By discovering the mechanisms of heat transport in these materials we aim to create new materials with exciting new thermal properties.",
             "Metal-organic-frameworks materials (MOFs) are an exciting new class of materials which are composed of a geometric molecular space-frame of organic linker molecules connecting metallic nodal units. These materials are open and intrinsically porous (the most porous materials known to man), however they also grow spontaneously in solution without the need of complex top-down assembly. There is a vast wealth of MOF crystal structures-a MOF's crystal symmetry is dictated by the metallic nodal units, and the topology by the geometry of the organic linker arms.",
 
@@ -37,40 +38,43 @@ var researchProjects = [
         ]
     },
     {
-        title : "Simulation of gecko-inspired dry adhesion system",
-        imageUrl: "http://research.engr.oregonstate.edu/greaney/sites/research.engr.oregonstate.edu.greaney/files/images/gecko_foot_on_glass-sm.preview.png",
+        title: "Simulation of gecko-inspired dry adhesion system",
+        href: "simulation-of-gecko-inspired-dry-adhesion-system",
+        imageUrl: "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/gecko_foot_on_glass-sm-600px.png",
         imageTitle: "Gecko foot on glass",
         teaser: "Write a sexy 1-2 sentence teaser that must tantalize the reader to make them follow the link and read more.",
-        text: [
+        expandedText: [
             "Geckos' amazing ability to walk upside down on the ceilings is due to the van der Waals attraction between the walls and millions of hairs on a gecko's foot. What is more remarkable, is that this dry adhesive is switchable -- geckos don't stick to walls like fly on fly-paper but can easily unstick their feet as they walk or run. In our research we are applying methods used to study earthquakes to discover the role of two factors in the gecko's adhesion system: the geometry of the hairs, and the hair to toe hierarchy."
         ]
     },
     {
-        title : "Dissipation in nanoscale resonators",
-        imageUrl: "http://research.engr.oregonstate.edu/greaney/sites/research.engr.oregonstate.edu.greaney/files/images/long-cnt-pulse-crp.thumbnail.png",
+        title: "Dissipation in nanoscale resonators",
+        href: "dissipation-in-nanoscale-resonators",
+        imageUrl: "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/poly-graphite-bg-orange-600px.png",
         imageTitle: "Wave packet in Carbon Nano Tubes",
         teaser: "Write a sexy 1-2 sentence teaser that must tantalize the reader to make them follow the link and read more.",
-        text: [
+        expandedText: [
             "Carbon nanotubes dissipate vibrations in surprising and unusual ways. The larger the excitation in a nanotube, the quicker it dissipates. We are keen to discover the cause of this strange dissipation and to find ways to make use to it.",
             "Carbon nanotubes (CNTs) are long and slender, lightweight, and incredibly stiff. This would make them ideal for nanoscale mechanical resonators (like tiny guitar strings) that resonate with exceedingly high frequency. Resonators such as this have many current and potential applications ranging from being able to weigh single atoms, to being used as emitters, receivers, and waveguides for wireless communication. "
         ]
     },
     {
-        title : "Some cool science thing",
+        title: "Some cool science thing",
+        href: "some-cool-science-thing",
         imageUrl: "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/allrules2.png",
         imageTitle: "Wave packet in Carbon Nano Tubes",
         teaser: "Write a sexy 1-2 sentence teaser that must tantalize the reader to make them follow the link and read more.",
-        text: [
+        expandedText: [
             "Blah blah blah blah nanotubes dissipate vibrations in surprising and unusual ways. The larger the excitation in a nanotube, the quicker it dissipates. We are keen to discover the cause of this strange dissipation and to find ways to make use to it.",
             "Carbon nanotubes (CNTs) are long and slender, lightweight, and incredibly stiff. This would make them ideal for nanoscale mechanical resonators (like tiny guitar strings) that resonate with exceedingly high frequency. Resonators such as this have many current and potential applications ranging from being able to weigh single atoms, to being used as emitters, receivers, and waveguides for wireless communication. "
         ]
     },
     {
         title: "Snappy title without jargon or overly technical words",
-        imageUrl: "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/matsci-tetrahedron.png",
+        imageUrl: "https://s3-us-west-2.amazonaws.com/alexgreaney.com/media/bubble6-600px.jpg",
         imageTitle: "Wave packet in Carbon Nano Tubes",
         teaser: "Write a sexy 1-2 sentence teaser that must tantalize the reader to make them follow the link and read more.",
-        text: [
+        expandedText: [
             "paragraph 1",
             "paragraph 2",
             "paragraph 3"
