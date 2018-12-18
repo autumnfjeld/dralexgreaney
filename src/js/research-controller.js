4d73c0d76ec6bc019994319277d9fd1c3d967ea7
Autumn Fjeld <autumn@ustwo.com>
2018-12-18 11:53:56 +1100

    WIP

--- src/js/research-controller.js
+++ src/js/research-controller.js
@@ -8,6 +8,7 @@
      * @constructor
      */
     function ResearchController(data, templatePage) {
+        console.log('ResearchController.templagePage()', data, templatePage);
         this.templatePage = templatePage;
         this.data = data;
     }
@@ -17,8 +18,7 @@
      */
     ResearchController.prototype.initView = function () {
         this._initBindings();
-        // this._initResearchPopup();
-
+        this._initResearchPopup();
     }; 
 
     /**
@@ -62,68 +62,71 @@
             },
             callbacks: {
                 elementParse: function (item) {
+                    console.log('elementParse  item', item)
                     var projectIndex = item.index;
-                    // console.log('ProjectIndex', projectIndex);
+                    console.log('ProjectIndex', projectIndex);
                     var project = $(item.el).attr('data-project');
-                    // console.log('data-project', $(item.el).attr('data-project'));
-                    // console.log('rawData', self.data, '\nthis data', self.data[projectIndex]);
+                    console.log('data-project', $(item.el).attr('data-project'));
+                    console.log('rawData', self.data, '\nthis data', self.data[projectIndex]);
+                    // Create the page with data
                     item.src = self.templatePage(self.data[projectIndex]);
                 }
             }
         });
     };
 
-    // ResearchController.prototype._initBindings = function () {
-    //     console.log('init bindings', this.templatePage);
-    //     var self = this;
-    //
-    //     var $mainPage = $('#main-page'),
-    //         $researchProjectPage = $('#research-project-page');
-    //
-    //     $('.research-projects').on('click', '.research-page-link', function (ev) {
-    //         var projectIndex = $(this).attr('data-project-index');
-    //         console.log(' clicked .research-page-link', projectIndex);
-    //         var projectTemplate = self.templatePage(self.data[projectIndex]);
-    //
-    //         var windowWidth = $(window).width(),
-    //             marginOffset = '-=' + 2*$(window).width();
-    //
-    //         $('#main-page').animate({
-    //             'margin-left': marginOffset
-    //         });
-    //         $('#main-page').hide();
-    //         // debugger;
-    //
-    //         var $researchProjectPage = $('#research-project-page');
-    //         var $researchProject = $('#research-project');
-    //         $('#research-project-page').append(projectTemplate);
-    //         // $researchProjectPage.css('margin-right', marginOffset);
-    //         $researchProject.show();
-    //         // $researchProjectPage.animate({
-    //         //     'margin-right': 0
-    //         // });
-    //
-    //         ev.preventDefault();
-    //
-    //     });
-    //
-    //     $('a.back').on('click', function(event) {
-    //         var windowWidth = $(window).width(),
-    //             marginOffset = '+=' + 2*$(window).width();
-    //
-    //         $researchProjectPage.animate({
-    //             'margin-right': marginOffset
-    //         });
-    //         $researchProject.remove();
-    //         $mainPage.animate({
-    //            'margin-left': 0
-    //         });
-    //
-    //         window.location.hash =  '#research';
-    //
-    //     });
-    //
-    // };
+    ResearchController.prototype._initBindings2 = function () {
+        console.log('init bindings', this.templatePage);
+        var self = this;
+    
+        var $mainPage = $('#main-page'),
+            $researchProjectPage = $('#research-project-page');
+    
+        // $('.research-projects').on('click', '.research-page-link', function (ev) {
+        $('#research').on('click', '.research-page-link', function (ev) {
+            var projectIndex = $(this).attr('data-project-index');
+            console.log(' clicked .research-page-link', projectIndex);
+            var projectTemplate = self.templatePage(self.data[projectIndex]);
+    
+            var windowWidth = $(window).width(),
+                marginOffset = '-=' + 2*$(window).width();
+    
+            $('#main-page').animate({
+                'margin-left': marginOffset
+            });
+            $('#main-page').hide();
+            // debugger;
+    
+            // var $researchProjectPage = $('#research-project-page');
+            var $researchProject = $('#research-project');
+            $('#research-project-page').append(projectTemplate);
+            // $researchProjectPage.css('margin-right', marginOffset);
+            $researchProject.show();
+            // $researchProjectPage.animate({
+            //     'margin-right': 0
+            // });
+    
+            ev.preventDefault();
+    
+        });
+    
+        $('a.back').on('click', function(event) {
+            var windowWidth = $(window).width(),
+                marginOffset = '+=' + 2*$(window).width();
+    
+            $researchProjectPage.animate({
+                'margin-right': marginOffset
+            });
+            $researchProject.remove();
+            $mainPage.animate({
+               'margin-left': 0
+            });
+    
+            window.location.hash =  '#research';
+    
+        });
+    
+    };
 
     // Export to window
     window.app = window.app || {};
