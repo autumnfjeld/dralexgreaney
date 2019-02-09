79e690cdf379f3a985b117cd9ec68cddaebe0943
Autumn Fjeld <autumn@ustwo.com>
2019-02-10 05:53:37 +1100

    Cleanup

--- src/js/research-controller.js
+++ src/js/research-controller.js
@@ -19,26 +19,29 @@
     ResearchController.prototype.initView = function () {
         this._initBindings();
         // TODO: wip, uncomment when ready for production
-        // this._initResearchPopup();
-    }; 
+        this._initResearchPopup();
+    };
 
     /**
-     * TODO Set up event bindings for Research UI
+     * Sets bindings for hovering over research images on desktop
+     * and tapping circle-i on touch devices
      * @private
      */
-    ResearchController.prototype._initBindings = function() {
+    ResearchController.prototype._initBindings = function () {
         // Assume device is non-touch screen and add hover class (could add in markup)
         $('#research .work-box').addClass('hover');
 
         // Listen for touch event to reset/setup DOM for touch device
-        window.addEventListener('touch-device-detected', function(ev) {
+        window.addEventListener('touch-device-detected', function (ev) {
             // This stops click event from registering twice
             ev.stopImmediatePropagation();
-            //Undo hover effect on the research teaser
+            // Undo hover effect on the research teaser
             $('#research .work-box').removeClass('hover');
             var $overlay = null;
+
             // TODO make whole work-box clickable - and rename to .teaser!
-            $('#research').on('click', '.btn-more-info', function() {
+            $('#research').on('click', '.btn-more-info', function () {
+                console.log('WHAT IS THIS?');
                 $overlay = $(this).parent().children('.work-box-overlay');
                 $overlay.toggleClass('show');
                 // TODO: toggle .fa-info with a .fa-close
@@ -68,7 +71,7 @@
                     console.log('ProjectIndex', projectIndex);
                     var project = $(item.el).attr('data-project');
                     // console.log('data-project', $(item.el).attr('data-project'));
-                    console.log('self.data', self.data,  projectIndex);
+                    console.log('self.data', self.data, projectIndex);
                     console.log('self.data[projectIndex]', self.data[projectIndex]);
                     // Create the page with data
                     item.src = self.templatePage(self.data[projectIndex]);
@@ -77,78 +80,64 @@
         });
     };
 
-    ResearchController.prototype._initBindings2 = function () {
-        console.log('init bindings', this.templatePage);
-        var self = this;
-    
-        var $mainPage = $('#main-page'),
-            $researchProjectPage = $('#research-project-page');
-    
-        // $('.research-projects').on('click', '.research-page-link', function (ev) {
-        $('#research').on('click', '.research-page-link', function (ev) {
-            var projectIndex = $(this).attr('data-project-index');
-            console.log(' clicked .research-page-link', projectIndex);
-            var projectTemplate = self.templatePage(self.data[projectIndex]);
-    
-            var windowWidth = $(window).width(),
-                marginOffset = '-=' + 2*$(window).width();
-    
-            $('#main-page').animate({
-                'margin-left': marginOffset
-            });
-            $('#main-page').hide();
-            // debugger;
-    
-            // var $researchProjectPage = $('#research-project-page');
-            var $researchProject = $('#research-project');
-            $('#research-project-page').append(projectTemplate);
-            // $researchProjectPage.css('margin-right', marginOffset);
-            $researchProject.show();
-            // $researchProjectPage.animate({
-            //     'margin-right': 0
-            // });
-    
-            ev.preventDefault();
-    
-        });
-    
-        $('a.back').on('click', function(event) {
-            var windowWidth = $(window).width(),
-                marginOffset = '+=' + 2*$(window).width();
-    
-            $researchProjectPage.animate({
-                'margin-right': marginOffset
-            });
-            $researchProject.remove();
-            $mainPage.animate({
-               'margin-left': 0
-            });
-    
-            window.location.hash =  '#research';
-    
-        });
-    
-    };
+    // WIP for a seperate reasearch page
+    // ResearchController.prototype._initBindings2 = function () {
+
+    //     console.log('ResearchController._initBindings2', this.templatePage);
+    //     debugger;
+    //     var self = this;
+
+    //     var $mainPage = $('#main-page'),
+    //         $researchProjectPage = $('#research-project-page');
+
+    //     // $('.research-projects').on('click', '.research-page-link', function (ev) {
+    //     $('#research').on('click', '.research-page-link', function (ev) {
+    //         var projectIndex = $(this).attr('data-project-index');
+    //         console.log(' clicked .research-page-link', projectIndex);
+    //         var projectTemplate = self.templatePage(self.data[projectIndex]);
+
+    //         var windowWidth = $(window).width(),
+    //             marginOffset = '-=' + 2 * $(window).width();
+
+    //         $('#main-page').animate({
+    //             'margin-left': marginOffset
+    //         });
+    //         $('#main-page').hide();
+    //         // debugger;
+
+    //         // var $researchProjectPage = $('#research-project-page');
+    //         var $researchProject = $('#research-project');
+    //         $('#research-project-page').append(projectTemplate);
+    //         // $researchProjectPage.css('margin-right', marginOffset);
+    //         $researchProject.show();
+    //         // $researchProjectPage.animate({
+    //         //     'margin-right': 0
+    //         // });
+
+    //         ev.preventDefault();
+
+    //     });
+
+    //     $('a.back').on('click', function (event) {
+    //         var windowWidth = $(window).width(),
+    //             marginOffset = '+=' + 2 * $(window).width();
+
+    //         $researchProjectPage.animate({
+    //             'margin-right': marginOffset
+    //         });
+    //         $researchProject.remove();
+    //         $mainPage.animate({
+    //             'margin-left': 0
+    //         });
+
+    //         window.location.hash = '#research';
+
+    //     });
+
+    // };
 
     // Export to window
     window.app = window.app || {};
     window.app.ResearchController = ResearchController;
 
 })(window);
-
-
-// (function (window) {
-//
-//     function ResearchModel() {
-//
-//     }
-//
-//     // Export to window
-//     window.app = window.app || {};
-//     window.app.ResearchModel = ResearchModel;
-//
-// })(window);
-//
-//
-
-
